55cab726156a5d8fd2be2448ddbe5558941ed2f3
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-03-18 14:40:27 -0400

    moving print menu

--- driver.cpp
+++ driver.cpp
@@ -14,7 +14,144 @@ int main(void)
     bool off = false;
 
     AmFmRadio radioAmFm(on); // Instantiate with default settings
-    radioAmFm.PrintMenu();// Radio object   
+    //radioAmFm;  
+
+	enum menuItems
+	{
+		kMenuNothing = 0,	// special constant for initialization only
+		kMenuTogglePower = 1, kMenuSetVolume, kMenuToggleAMFM, kMenuSetFreq, kMenuSelectFreq,
+		kMenuShowCurrentSettings, kMenuScanUp, kScanDown, kMenuQuit
+	};
+
+	menuItems	choice = kMenuNothing;
+
+	int			volStore = 0;
+	int			freqStore = 0;
+	int			freq_num = 0;
+	char		buf[kSize] = { 0 };
+	//bool		on = false;
+
+	do
+	{
+		printf("\n\nMAIN MENU\n");
+		printf("=========\n\n");
+		printf("1.  Toggle Power\n");
+		printf("2.  Set Volume\n");
+		printf("3.  Toggle AM / FM Band\n");
+		printf("4.  Set a Preset Freq \n");
+		printf("5.  Select a Preset Freq \n");
+		printf("6.  Show Current Settings\n");
+		printf("7.  Scan Up \n");
+		printf("8.  Scan Down\n");
+		printf("9.  Quit the Program\n");
+		printf("\nMake a selection from the menu : ");
+		fgets(buf, sizeof buf, stdin);
+		choice = (menuItems)atoi(buf);
+
+		switch (choice)
+		{
+		case kMenuTogglePower:
+			radioAmFm.PowerToggle();
+			break;
+		case kMenuSetVolume:
+			volStore = radioAmFm.SetVolume();
+			if (volStore == kMinVol)
+			{
+				printf("\nVolume was set to 0.");
+			}
+			else if (volStore == kSetToMax)
+			{
+				printf("\nVolume was set to 100.");
+			}
+			break;
+
+		case kMenuToggleAMFM:
+			on = radioAmFm.IsRadioOn();
+			if (on)
+			{
+				radioAmFm.ToggleBand();
+			}
+			else
+			{
+				printf("\nThe radio must be turned on before the "
+					"band can be toggled!");
+			}
+			break;
+		case kMenuSetFreq:
+			on = radioAmFm.IsRadioOn();
+			if (on)
+			{
+				printf("\nWhich frequency do you want to set?");
+				fgets(buf, sizeof buf, stdin);
+				freq_num = atoi(buf) kNull;
+				freqStore = radioAmFm.SetPresetFreq(freq_num);
+				if (freqStore == 0)
+				{
+					printf("\nYou entered an invalid frequency number!");
+				}
+			}
+			else
+			{
+				printf("\nYou must turn the radio on before you set the frequency!\n");
+			}
+			break;
+		case kMenuSelectFreq:
+			on = radioAmFm.IsRadioOn();
+			if (on)
+			{
+				printf("\nEnter the number of the preset frequency: ");
+				fgets(buf, kSize, stdin);
+				freq_num = atoi(buf) kNull;
+				freqStore = radioAmFm.SelectPresetFreq(freq_num);
+				if (freqStore == 0)
+				{
+					printf("\nYou entered an invalid frequency number!");
+				}
+			}
+			else
+			{
+				printf("\nYou must turn the radio on before you set the frequency!\n");
+			}
+			break;
+		case kMenuShowCurrentSettings:
+			radioAmFm.ShowCurrentSettings();
+			break;
+		case kMenuScanUp:
+			on = radioAmFm.IsRadioOn();
+			if (on)
+			{
+				radioAmFm.ScanUp();
+			}
+			else
+			{
+				printf("\nYou must turn the radio on before you can use the scan up frequency!\n");
+			}
+			break;
+		case kScanDown:
+			on = radioAmFm.IsRadioOn();
+			if (on)
+			{
+				radioAmFm.ScanDown();
+			}
+			else
+			{
+				printf("\nYou must turn the radio on before you can use the scan down frequency!\n");
+			}
+			break;
+		case kMenuQuit:
+			printf("\nGoodbye!\n");
+			radioAmFm.~AmFmRadio();
+			exit(kSuccess);
+		default:
+			printf("You have entered an invalid selection. Please make \n"
+				"another selection.\n");
+			break;
+		}
+
+		if ((choice != kMenuShowCurrentSettings) && (choice != kMenuQuit))
+			radioAmFm.ShowCurrentSettings();
+
+	} while (choice != kMenuQuit);
 
     return 0;
 }
