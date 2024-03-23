bfdff37efa66d7fe714475d8785defd9390bec8f
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-03-23 17:25:07 -0400

    carDriver.cpp

---
+++ carDriver.cpp
@@ -0,0 +1,25 @@
+/* -- FILEHEADER COMMENT --
+   NAME    : carDriver.cpp
+   PURPOSE : This header file contains the declaration of the PioneerCarRadio
+   sub-class, which represents an AM/FM radio device. It provides functionalities
+   to toggle power, adjust volume, set and select preset frequencies, Choose 
+   button radio stations.
+*/
+#include "AmFmRadio.h"
+#include "PioneerCarRadio.h"
+
+
+int main(void)
+{
+	bool off = true;
+	PioneerCarRadio RadioPioneer(off);
+
+	do
+	{
+		system("cls");
+		RadioPioneer.KeystrokeHandling();		
+
+	} while (1);
+			
+	return 0;
+}
